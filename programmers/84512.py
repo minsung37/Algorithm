92cf8fd9b281dafee37289c48ab54fb7a25021be
minsung37 <tampang99@naver.com>
2022-08-11 02:20:45 +0900

    모음사전

--- programmers/84512.py
+++ programmers/84512.py
@@ -13,3 +13,18 @@ def solution(word):
     dfs(0, "")
     words.sort()
     return words.index(word) + 1
+
+
+# 모음 사전 두번째 풀이
+def solution2(word):
+    alpa = ["A", "E", "I", "O", "U"]
+    dic = []
+
+    def dfs(word):
+        if len(word) == 5:
+            return
+        for i in alpa:
+            dic.append(word + i)
+            dfs(word + i)
+    dfs("")
+    return dic.index(word) + 1
