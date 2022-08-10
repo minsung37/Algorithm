532b2d7162caa7810eaee18ad223059732e0e157
minsung37 <tampang99@naver.com>
2022-08-11 01:54:12 +0900

    단어변환

--- programmers/43163.py
+++ programmers/43163.py
@@ -1,5 +1,31 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43163
 # 단어 변환
+from collections import deque
+
+
 def solution(begin, target, words):
-    answer = 0
-    return answer
+    def is_next(begin, target):
+        count = 0
+        for i in range(len(begin)):
+            if begin[i] == target[i]:
+                count = count + 1
+        return count
+
+    queue = deque()
+    queue.append((begin, 0))
+    y = 0
+    if target not in words:
+        return y
+    while queue:
+        x, y = queue.popleft()
+        if x == target:
+            return y
+        for i in words:
+            if is_next(x, i) == len(begin) - 1:
+                queue.append((i, y + 1))
+                words.remove(i)
+    return y
+
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]))
