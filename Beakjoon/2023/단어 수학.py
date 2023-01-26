17a65eac1e4166632cd792c232f53a9c64dd35a9
minsung37 <tampang99@naver.com>
2023-01-26 23:33:19 +0900

    G4_단어 수학

---
+++ Beakjoon/2023/단어 수학.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+words = []
+for _ in range(n):
+    temp = input().rstrip()
+    words.append(temp)
+
+dic = defaultdict(int)
+for word in words:
+    for index, alphabet in enumerate(word):
+        dic[alphabet] = dic[alphabet] + 10 ** (len(word) - 1 - index)
+# value 기준 정렬
+alpha_sort = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+
+result, number = 0, 9
+for i in alpha_sort:
+    result = result + number * i[1]
+    number = number - 1
+print(result)
+
+
+
