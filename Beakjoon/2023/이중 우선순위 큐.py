c157f5c4b348192cf837668de3b722eae10d81ea
minsung37 <tampang99@naver.com>
2023-01-29 02:14:36 +0900

    G4_이중 우선순위 큐

---
+++ Beakjoon/2023/이중 우선순위 큐.py
@@ -0,0 +1,43 @@
+import heapq
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    dic = defaultdict(int)
+    n = int(input())
+    heap_min, heap_max = [], []
+    count = 0
+    for _ in range(n):
+        commend, number = map(str, input().split())
+        number = int(number)
+        if commend == "I":
+            dic[number] = dic[number] + 1
+            count = count + 1
+            heapq.heappush(heap_min, number)
+            heapq.heappush(heap_max, -number)
+        elif commend == "D":
+            if count == 0:
+                continue
+            count = count - 1
+            if number == -1:
+                if heap_min:
+                    k = heapq.heappop(heap_min)
+                    dic[k] = dic[k] - 1
+            else:
+                if heap_max:
+                    k = heapq.heappop(heap_max)
+                    dic[-k] = dic[-k] - 1
+        while heap_min and dic[heap_min[0]] == 0:
+            heapq.heappop(heap_min)
+        while heap_max and dic[-heap_max[0]] == 0:
+            heapq.heappop(heap_max)
+    if count == 0:
+        print("EMPTY")
+    else:
+        result = []
+        for i in dic:
+            if dic[i] > 0:
+                result.append(i)
+        print(max(result), min(result))
