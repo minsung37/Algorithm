0472669a6499d372434e8770b8ac5e5cd4817b17
minsung37 <tampang99@naver.com>
2023-06-03 09:39:57 +0900

    L2_혼자서 하는 틱택토

---
+++ programmers/L2/L2_혼자서 하는 틱택토.py
@@ -0,0 +1,9 @@
+def solution(board):
+    answer = -1
+    return answer
+
+
+print(solution(["O.X", ".O.", "..X"]))
+print(solution(["OOO", "...", "XXX"]))
+print(solution(["...", ".X.", "..."]))
+print(solution(["...", "...", "..."]))
